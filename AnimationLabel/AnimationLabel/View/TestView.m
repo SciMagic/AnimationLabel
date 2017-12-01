0765ae5209ce02e759c4ab08d7d4c30ff97836f7
超八机 <chaobaji@vip.qq.com>
2017-12-01 14:19:57 +0800

    test

---
+++ AnimationLabel/AnimationLabel/View/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  AnimationLabel
+//
+//  Created by QD-ZC on 2017/12/1.
+//  Copyright © 2017年 Hacky. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
